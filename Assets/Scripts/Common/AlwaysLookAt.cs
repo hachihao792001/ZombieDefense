ebd66d24f7d619c80c0d1ee7432eb2b721db3f57
hachihao792001 <hachihao792001@gmail.com>
2022-07-10 23:59:10 +0700

    camera rotate around and look at the middle of the main area in main menu

---
+++ Assets/Scripts/Common/AlwaysLookAt.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysLookAt : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+    void Update()
+    {
+        transform.LookAt(_target);
+    }
+}
